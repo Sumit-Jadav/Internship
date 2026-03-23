0310eab191e3fbd3f3be24bb7302c540a5b75971
Jadav Sumit <169023435+Sumit-Jadav@users.noreply.github.com>
2026-03-23 23:11:40 +0530

    Add files via upload

--- job_crud/index.js
+++ job_crud/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 let db = null;
 // Getting database reference
 async function startServer() {
